08820ac4772e0a007878dad5fe5dfde4fea0577e
guiddone <guiddo_gnr@hotmail.com>
2021-06-23 13:09:23 -0300

    --webprofiles, debug, logger, maker, annotations

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="conference")
+     */
+    public function index(): Response
+    {
+        return $this->render('conference/index.html.twig');
+       
+    }
+}
